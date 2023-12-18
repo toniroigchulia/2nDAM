c9e0f2f878d0bb9b652e7079e39aed978151a1fc
ToniPc <toni196146@gmail.com>
2023-12-18 19:44:05 +0100

    ViewerCosas

--- Interfaces/ThreadLaboratory/ThreadLaboratory/src/Viewer.java
+++ Interfaces/ThreadLaboratory/ThreadLaboratory/src/Viewer.java
@@ -4,44 +4,40 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import javax.swing.JFrame;
 
-public class Viewer extends JFrame implements ComponentListener, ActionListener, ItemListener, Runnable{
+public class Viewer extends JFrame implements Runnable {
     private LaboratoryController controller;
-    
+
     private DTOLabParameters labParameters;
-    
+    private ActionListener listener;
     private ControlPanel controlPanel;
     private ParameterPanel parameterPanel;
     private ResultatPanel resultatPanel;
-    
+
     // METODOS CONSTRUCTORES
     public Viewer(LaboratoryController controller) {
         this.controller = controller;
+        this.listener = new Listener();
         this.configureJFrame();
     }
-    
+
     // METODOS
     // metodo que añade las componentes del JFrame
     private void addUIComponents() {
         Container panel;
-        
+
         panel = this.getContentPane();
         this.addComponents(panel);
     }
-    
+
     // metood que configura la estructura del JFrame
     private void configureJFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new GridBagLayout());
-        this.addComponentListener(this);
-        this.setSize(920, 520);
         this.setVisible(true);
         this.addUIComponents();
+        this.setSize(920, 520);
     }
 
     // metodo que une todo para gestionar la union entre todas las clases
@@ -53,7 +49,7 @@ public class Viewer extends JFrame implements ComponentListener, ActionListener,
 
         c.gridy = 0;
         c.gridx = 0;
-        this.controlPanel = new ControlPanel();
+        this.controlPanel = new ControlPanel(listener);
         panel.add(this.controlPanel, c);
 
         c.gridx = 1;
@@ -61,16 +57,15 @@ public class Viewer extends JFrame implements ComponentListener, ActionListener,
         panel.add(this.resultatPanel, c);
 
         c.gridx = 2;
-        this.parameterPanel = new ParameterPanel();
+        this.parameterPanel = new ParameterPanel(new DTOLabParameters());
         panel.add(this.parameterPanel, c);
     }
 
     @Override
     public void run() {
         try {
-            while(true){
+            while (true) {
                 this.resultatPanel.setLabResults(this.controller.getResults());
-                this.controller.applyConfig(this.labParameters);
                 Thread.sleep(50);
             }
         } catch (Exception e) {
@@ -78,33 +73,102 @@ public class Viewer extends JFrame implements ComponentListener, ActionListener,
         }
     }
 
-    @Override
-    public void itemStateChanged(ItemEvent e) {
-        // throw new UnsupportedOperationException("Unimplemented method 'itemStateChanged'");
+    private class Listener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String str = e.getActionCommand();
+            switch (str) {
+                case "Play":
+                    labParameters.setPlaying(true);
+
+                    controller.applyConfig(labParameters);
+                    //System.out.println("Play");
+                    break;
+                case "Pause":
+                    labParameters.setPlaying(false);
+
+                    controller.applyConfig(labParameters);
+                    //System.out.println("Pause");
+                    break;
+                case "Aplicar":
+                    labParameters.setProductores(Integer.parseInt(getParameterPanel().getTextProductors().getText()));
+                    labParameters.setQuantityItemsP(
+                            Integer.parseInt(getParameterPanel().getTextQuanItemProductor().getText()));
+                    labParameters.setTempsMaximCheckBoxP(getParameterPanel().getTempsProduccio().isSelected());
+                    labParameters.setTempsMaximP(
+                            Integer.parseInt(getParameterPanel().getTextTempsMaximProduccio().getText()));
+
+                    labParameters.setConsumidores(Integer.parseInt(getParameterPanel().getTextConsumidors().getText()));
+                    labParameters.setQuantityItemsC(
+                            Integer.parseInt(getParameterPanel().getTextQuantItemConsumidor().getText()));
+                    labParameters.setTempsMaximCheckBoxC(getParameterPanel().getTempsConsumir().isSelected());
+                    labParameters.setTempsMaximC(
+                            Integer.parseInt(getParameterPanel().getTextTempsMaximConsumir().getText()));
+
+                    labParameters.setProtegerRegCritic(getControlPanel().getProtRegCrit().isSelected());
+                    labParameters.setStockPositivo(getControlPanel().getStockPositive().isSelected());
+
+                    controller.applyConfig(labParameters);
+                    // System.out.println("Aplicar");
+                    break;
+                case "Reiniciar":
+
+                    getParameterPanel().setLabParameters(new DTOLabParameters());
+                    getParameterPanel().setDefaultFieldsValues();
+                    controller.applyConfig(labParameters);
+                    // System.out.println("Reiniciar");
+                    break;
+                default:
+                    System.err.println("Acción NO tratada: " + e);
+            }
+        }
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    public LaboratoryController getController() {
+        return controller;
     }
 
-    @Override
-    public void componentResized(ComponentEvent e) {
-        // throw new UnsupportedOperationException("Unimplemented method 'componentResized'");
+    public void setController(LaboratoryController controller) {
+        this.controller = controller;
     }
 
-    @Override
-    public void componentMoved(ComponentEvent e) {
-        // throw new UnsupportedOperationException("Unimplemented method 'componentMoved'");
+    public DTOLabParameters getLabParameters() {
+        return labParameters;
     }
 
-    @Override
-    public void componentShown(ComponentEvent e) {
-        // throw new UnsupportedOperationException("Unimplemented method 'componentShown'");
+    public void setLabParameters(DTOLabParameters labParameters) {
+        this.labParameters = labParameters;
     }
 
-    @Override
-    public void componentHidden(ComponentEvent e) {
-        // throw new UnsupportedOperationException("Unimplemented method 'componentHidden'");
+    public ActionListener getListener() {
+        return listener;
+    }
+
+    public void setListener(ActionListener listener) {
+        this.listener = listener;
+    }
+
+    public ControlPanel getControlPanel() {
+        return controlPanel;
+    }
+
+    public void setControlPanel(ControlPanel controlPanel) {
+        this.controlPanel = controlPanel;
+    }
+
+    public ParameterPanel getParameterPanel() {
+        return parameterPanel;
+    }
+
+    public void setParameterPanel(ParameterPanel parameterPanel) {
+        this.parameterPanel = parameterPanel;
+    }
+
+    public ResultatPanel getResultatPanel() {
+        return resultatPanel;
+    }
+
+    public void setResultatPanel(ResultatPanel resultatPanel) {
+        this.resultatPanel = resultatPanel;
     }
 }
