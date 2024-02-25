350c251f8e930cb89d122b8735df50a56e8815c1
ToniPc <toni196146@gmail.com>
2024-02-25 18:20:09 +0100

    CambiosDeUltimoMinuto

--- Interfaces/BallsGame/BallsGame/src/Engine/TGLocalController.java
+++ Interfaces/BallsGame/BallsGame/src/Engine/TGLocalController.java
@@ -28,113 +28,14 @@ public class TGLocalController {
         // Colision contra otras bolas
         if (ballsColiding.size() > 0) {
             for (int i = 0; i < ballsColiding.size(); i++) {
-                ballToBallBounce(mainBall, ballsColiding.get(i));
+                this.rules.ballToBallBounce(mainBall, ballsColiding.get(i));
             }
         }
 
         // Colision contra bordes
-        if (borderBounce(mainBall)) {
+        if (this.rules.borderBounce(mainBall)) {
             mainBall.move();
         }
-    }
-
-    private void ballToBallBounce(Ball mainBall, Ball otherBall) {
-        // Si tienen direcciones contrarias
-        if ((mainBall.getVelocity().getX() > 0 && otherBall.getVelocity().getX() < 0)
-                || (mainBall.getVelocity().getX() < 0 && otherBall.getVelocity().getX() > 0)) {
-
-            mainBall.getVelocity().setX(mainBall.getVelocity().getX() * (-1));
-            otherBall.getVelocity().setX(otherBall.getVelocity().getX() * (-1));
-        }
-
-        if ((mainBall.getVelocity().getX() > 0 && otherBall.getVelocity().getY() < 0)
-                || (mainBall.getVelocity().getY() < 0 && otherBall.getVelocity().getY() > 0)) {
-
-            mainBall.getVelocity().setY(mainBall.getVelocity().getY() * (-1));
-            otherBall.getVelocity().setY(otherBall.getVelocity().getY() * (-1));
-        }
-
-        // Si las bolas tienen la misma direccion
-        if ((mainBall.getVelocity().getX() > 0 && otherBall.getVelocity().getX() > 0)
-                || (mainBall.getVelocity().getX() < 0 && otherBall.getVelocity().getX() < 0)) {
-
-            mainBall.getVelocity().setX(mainBall.getVelocity().getX() * (-1));
-        }
-
-        if ((mainBall.getVelocity().getY() > 0 && otherBall.getVelocity().getY() > 0)
-                || (mainBall.getVelocity().getY() < 0 && otherBall.getVelocity().getY() < 0)) {
-
-            mainBall.getVelocity().setY(mainBall.getVelocity().getY() * (-1));
-        }
-        
-        // Si una de las velocidades es 0
-        if (otherBall.getVelocity().getX() == 0) {
-            
-            mainBall.getVelocity().setX(mainBall.getVelocity().getX() * (-1));
-        }
-        
-        if (otherBall.getVelocity().getY() == 0) {
-            
-            mainBall.getVelocity().setY(mainBall.getVelocity().getY() * (-1));
-        }
-    }
-
-    private boolean borderBounce(Ball mainBall) {
-        // Colision contra bordes
-        Vector<Integer> canvaSize = this.getCanvasSize();
-        CoordinatesDTO bouncePosition = new CoordinatesDTO();
-
-        // Bordes Laterales
-        if (mainBall.getNextPosition().getX() <= 0) {
-
-            if (!this.rules.checkGate(2)) {
-
-                bouncePosition.setX(0);
-                bouncePosition.setY(mainBall.getNextPosition().getY());
-
-                mainBall.bounce(bouncePosition, 0);
-                return true;
-            } else {
-
-                sendObject(mainBall, PeerLocation.WEST);
-                return false;
-            }
-        } else if (mainBall.getNextPosition().getX() >= canvaSize.get(0)) {
-
-            if (!this.rules.checkGate(1)) {
-
-                bouncePosition.setX(canvaSize.get(0));
-                bouncePosition.setY(mainBall.getNextPosition().getY());
-
-                mainBall.bounce(bouncePosition, 0);
-                return true;
-            } else {
-
-                sendObject(mainBall, PeerLocation.EAST);
-                return false;
-            }
-        }
-
-        // Techo y Suelo
-        if (mainBall.getNextPosition().getY() <= 0) {
-
-            bouncePosition.setX(mainBall.getNextPosition().getX());
-            bouncePosition.setY(0);
-
-            mainBall.bounce(bouncePosition, 1);
-
-            return true;
-        } else if (mainBall.getNextPosition().getY() >= canvaSize.get(1)) {
-
-            bouncePosition.setX(mainBall.getNextPosition().getX());
-            bouncePosition.setY(canvaSize.get(1));
-
-            mainBall.bounce(bouncePosition, 1);
-
-            return true;
-        }
-
-        return true;
     }
 
     public Vector<Integer> getCanvasSize() {
